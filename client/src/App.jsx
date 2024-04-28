5dfe6c89ec90d87b0021d80ff0d6672982ef7313
ThisIsSagarKaushik <sagarkaushik28may@gmail.com>
2024-04-29 02:04:14 +0530

    adding routes and create components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import Home from "./Pages/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Profile from "./Pages/Profile";
 
 export default function() {
   return (
-    <div><h1 className='text-red-500'>Tailwind</h1></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
